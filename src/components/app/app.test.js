92a4abb2ed294a771b663be9e10b42c771c8fa4c
Yuriy Kurant <yuriy.kurant@gmail.com>
2019-08-23 02:35:46 +0300

    HT2: tests added

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import App from './app';
-import {restaurants} from '../../fixtures';
+import React from "react";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./app";
+import { restaurants } from "../../fixtures";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('At starts', function () {
-  it('should show all restaurants', function () {
-    const wrapper = mount(<App restaurants={restaurants} />)
-    expect(wrapper.find('li[data-autoid="RESTAURANT_ITEM"]').length).toEqual(4)
+describe("At starts", function() {
+  it("should show all restaurants", function() {
+    const wrapper = mount(<App restaurants={restaurants} />);
+    expect(wrapper.find('li[data-autoid="RESTAURANT_ITEM"]').length).toEqual(4);
   });
 
-  describe('when click on Open menu button', function () {
-    it('should show menu only for one restaurant', function () {
-      const wrapper = mount(<App restaurants={restaurants} />)
-      const id = restaurants[0].id
-      wrapper
-        .find(`button[data-autoid="OPEN_MENU_ITEM_${id}"]`)
-        .simulate('click')
-      expect(wrapper.find(`div[data-autoid="MENU_ITEMS_${id}"]`).length).toEqual(1)
+  describe("toggle menu button", function() {
+    it("should show/hide menu only for one restaurant after click on Open/Close menu", function() {
+      const wrapper = mount(<App restaurants={restaurants} />);
+      const id = restaurants[0].id;
+      const btn = wrapper.find(`button[data-autoid="OPEN_MENU_ITEM_${id}"]`);
+
+      btn.simulate("click");
+
+      expect(wrapper.find(`div[data-autoid="MENU_ITEMS_${id}"]`).length).toBe(
+        1
+      );
+
+      expect(btn.text()).toEqual("Close menu");
+      btn.simulate("click");
+      expect(wrapper.find(`div[data-autoid="MENU_ITEMS_${id}"]`).length).toBe(
+        0
+      );
     });
   });
 
-  describe('when App get fetchData prop', function () {
-    it('should call fetchData callback', function (done) {
-      mount(<App
-        restaurants={restaurants}
-        fetchData={() => done()}
-      />)
+  describe("when App get fetchData prop", function() {
+    it("should call fetchData callback", function(done) {
+      mount(<App restaurants={restaurants} fetchData={() => done()} />);
     });
   });
-
 });
--- src/components/dish/dish.js
+++ src/components/dish/dish.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { Card, Button } from "antd"
-import {useCounter} from '../../custom-hooks/use-counter'
+import React from "react";
+import { Card, Button } from "antd";
+import { useCounter } from "../../custom-hooks/use-counter";
 
 function Dish(props) {
   const [amount, decrease, increase] = useCounter(0);
@@ -10,23 +10,31 @@ function Dish(props) {
       actions={[
         `£${props.price}`,
         <>
-          <span style={{ margin: "0 12px" }}>{amount}</span>
+          <span
+            data-autoid={`AMOUNT_DISH_${props.id}`}
+            style={{ margin: "0 12px" }}
+          >
+            {amount}
+          </span>
           <Button.Group>
             <Button
               onClick={decrease}
               type="primary"
               shape="circle"
               icon="minus"
+              data-autoid="DECREASE_BTN"
             />
             <Button
               onClick={increase}
               type="primary"
               shape="circle"
               icon="plus"
+              data-autoid="INCREASE_BTN"
             />
           </Button.Group>
         </>
       ]}
+      data-autoid={`DISH_ITEM_${props.id}`}
     >
       <Card.Meta
         title={props.name}
